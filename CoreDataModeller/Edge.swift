bf5eb7737bb4398eaaf26ab53ac57497f1cd4615
David Marquis <davidmarquis@Kristens-MacBook-Pro.local>
2015-05-28 13:19:50 -0400

    Fix corrupt index

---
+++ CoreDataModeller/Edge.swift
@@ -0,0 +1,13 @@
+//
+//  Edge.swift
+//  CoreDataModeller
+//
+//  Created by David Marquis on 2015-05-28.
+//  Copyright (c) 2015 David Marquis. All rights reserved.
+//
+
+import UIKit
+
+class Edge: NSObject {
+   
+}
---
+++ CoreDataModeller/GraphHeader.m
@@ -0,0 +1,9 @@
+//
+//  GraphHeader.m
+//  CoreDataModeller
+//
+//  Created by David Marquis on 2015-05-28.
+//  Copyright (c) 2015 David Marquis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
