cb13b4aafb03b97c8c62c83d63a6f44cb2ae888f
David Marquis <david.a.marquis@gmail.com>
2015-06-07 22:15:51 -0400

    Add view methods in swift

--- CoreDataModeller/GraphView.swift
+++ CoreDataModeller/GraphView.swift
@@ -10,12 +10,33 @@ import UIKit
 
 class GraphView: UIScrollView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+//
+//  VertCircle.m
+//  April25NotGoingWell
+//
+//  Created by David Marquis on 2015-04-26.
+//  Copyright (c) 2015 David Marquis. All rights reserved.
+//
+
+    lazy var gwv:GraphWorldView = {
+        // inside a block so we have to use self
+            let sizeX:CGFloat = self.contentSize.width;
+            let sizeY:CGFloat = self.contentSize.height;
+            let worldRect:CGRect = CGRectMake(0,0,sizeX,sizeY);
+            let worldView = init();
+            worldView.frame=worldRect
+            addSubview(worldView);
+            return worldView;
+    }()
+    
+    required init(coder aDecoder: NSCoder) {
+        // set the content size first as all subclasses will need it
+        self.contentSize=CGSizeMake(1000,1000);
+        // self.backgroundColor may seem irrelevant as the view is covered by a GraphWorldView
+        // This is wrong. There is no default color so nothing will be displayed
+        self.backgroundColor=UIColor.whiteColor();
+        self.opaque=false;
+        super.init(coder: aDecoder);
     }
-    */
 
 }
--- CoreDataModeller/GraphWorldView.swift
+++ CoreDataModeller/GraphWorldView.swift
@@ -10,12 +10,100 @@ import UIKit
 
 class GraphWorldView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+override init(frame: CGRect) {
+    self.backgroundColor=UIColor.whiteColor();
+    self.circSize=50;
+    self.edgeSize=10;
+    self.strokeSize=5;
+    // make the graph recognize taps
+    let recog      = UITapGestureRecognizer(target:self, action:"tap:");
+    addGestureRecognizer(recog);
+    super.init(frame: frame);
+}
+
+required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder);
+}
+
+var circSize:Double;
+var edgeSize:Double;
+var strokeSize:Double;
+
+func removeSubviews() {
+    for s in subviews {
+        s.removeFromSuperview();
     }
-    */
+}
+
+var radius:Double {return (self.circSize+self.edgeSize)/2; }
+var diameter:Double {return (self.circSize+self.edgeSize); }
+
+func tap(recognizer:UITapGestureRecognizer) {
+
+    let cgp:CGPoint=recognizer.locationInView(self);
+    let N:Int=subviews.count;
+    var i:Int=0;
+    
+    for(i=0;i<N;i++) {
+    
+        if(subviews[i] is VertView) {
+            let cur:VertView = subviews[i] as! VertView;
+            // do a hit test on the vert
+            if(cur.updateIfHit(cgp)) {
+                break;
+            }
+        }
+    }
+    if(i+1==N) {
+        return;
+    }
+    else {
+        i=i+1;
+        for( ;i<N;i++) {
+            if(subviews[i] is VertView) {
+                let cur:VertView=subviews[i] as! VertView;
+                if(cur.selected) {
+                    cur.selected=false;
+                }
+            }
+        }
+    }
+}
+
+-(VertView*)addVertAtPoint:(CGPoint) cgp {
+    double newX=cgp.x;
+    double newY=cgp.y;
+    
+    VertView* vert=[[VertView alloc] initWithFrame:CGRectMake(newX, newY, self.circSize+self.edgeSize, self.circSize+self.edgeSize)];
+
+    vert.circSize=self.circSize;
+    vert.strokeSize=self.strokeSize;
+    [self addSubview:vert];
+    [self setNeedsDisplay];
+    return vert;
+}
+
+func addEdgeWithFrame(frame:CGRect, edgeDirectionCase:Int) {
+    EdgeView* ev=[[EdgeView alloc] initWithFrame:frame];
+    ev.directionCase=edgeDirectionCase;
+    // fill in the radius
+    ev.radius=self.radius;
+    [self addSubview:ev];
+    [self sendSubviewToBack:ev];
+}
+
+// public
+// return the VertView corresponding to a particular id
+// or nil if such a VertView does not exist
+-(VertView*) getVertViewById:(NSNumber*)vertViewId {
+
+    for(id subview in self.subviews) {
+        if([subview isKindOfClass:[VertView class]]) {
+            VertView* vertView=subview;
+            if([vertView.vertViewId isEqualToNumber:vertViewId]) {return vertView;}
+        }
+    }
+    return nil;
+}
 
 }
